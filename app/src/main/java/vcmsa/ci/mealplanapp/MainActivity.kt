5c31b8cb0771ad31e07692257eb8134d676cde98
ST10481235 <ST10481235@vcconnect.edu.za>
2025-04-02 20:37:29 +0200

    initial code

--- app/src/main/java/vcmsa/ci/mealplanapp/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/mealplanapp/MainActivity.kt
@@ -1,18 +1,11 @@
-/*
-
- */
-
 package vcmsa.ci.mealplanapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
